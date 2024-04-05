07bda87af748a3cfc7e99c343736f40b0413ba55
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-05 11:31:55 +0200

    feat: process comments

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Funzione genera un numero casuale tra 1 e 100 (inclusi)
+// Chiedi all'utente di inserire un numero N
+// Verifica che N sia valido oppure ès tato inserito un valore non number
+// Genera un array casuale di 10 numeri e stampalo a schermo
