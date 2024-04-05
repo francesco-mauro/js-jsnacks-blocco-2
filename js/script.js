360b7bfecb6eda632b1d202e3b30624b3142d419
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-05 11:39:05 +0200

    add: prototype function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 // Chiedi all'utente di inserire un numero N
 // Verifica che N sia valido oppure ès tato inserito un valore non number
 // Genera un array casuale di 10 numeri e stampalo a schermo
+
+function generaArrayCasuale(dimensioneCasuale) {
+    let arr = [];
+    for (let i = 0; i < dimensioneCasuale; i++) {
+        arr.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return arr;
+}
