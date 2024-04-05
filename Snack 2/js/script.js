389f9bef4ba1001bfad7e00e38bd94f05c437769
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-05 12:32:20 +0200

    add: word analisis of procedure for snack 2

---
+++ Snack 2/js/script.js
@@ -0,0 +1,7 @@
+// Scrivere unaFunzione che confronta le lunghezze delle parole e stampa il risultato
+    // Confronto con if
+        // Se le parole hanno la stessa lunghezza, stampa entrambe
+        // Altrimenti, stampa la parola più lunga
+    // Esegui la funzione con le due parole inserite dall'utente
+
+// Chiede all'utente di inserire due parole
